78ae5bf4b28c3f9224c07ebf9d9112f0bdbdfb17
Michael Boroski <mjboroski@gmail.com>
2018-08-05 20:58:28 +0000

    automatically backed up by learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,9 +1,11 @@
 // ./src/containers/MoviesPage.js
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
+import MovieShow from './MovieShow';
 
-const MoviesPage = ({ movies }) => 
+const MoviesPage = ({ movies }) =>
   <div>
     <MoviesList movies={movies} />
   </div>;
